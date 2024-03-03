d4211eaab8b511ebfe2a274fada671efed73c790
shreyans13 <shreyans1313@gmail.com>
2024-03-04 02:43:37 +0530

    moved to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['selector', '[data-mode="dark"]'],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    // backgroundColor: "#000000",
+    extend: {
+      fontFamily: {
+        FranchiseFree: ["FranchiseFree"],
+        JosefinSlab: ["JosefinSlab"]
+      },
+      colors: {
+        backgroundColor: "var(--color-background)",
+        textBackground: "var(--text-background)"
+      },
+
+      backgroundColor: '#000000',
+      fontSize: {
+        '10xl': '10rem',
+        '11xl': '12rem',
+        '12xl': '14rem',
+        '13xl': '16rem',
+      },
+
+    },
   },
   plugins: [],
 }
