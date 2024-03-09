cbe54f8a04bd983e420dd2e3f11c776ff73fa876
shreyans13 <shreyans1313@gmail.com>
2024-03-09 16:49:14 +0530

    added tailwind:prettier:linter

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,8 @@ import "./Header.scss";
 function Header() {
   return (
     <>
-      <header className="z-30 flex items-center w-full h-24 sm:h-32">
-        <div className="container flex items-center justify-between lg:justify-center px-6 mx-auto">
+      <header className="z-30 flex h-24 w-full items-center sm:h-32">
+        <div className="container mx-auto flex items-center justify-between px-6 lg:justify-center">
           <div className="flex items-center text-3xl font-black uppercase">
             <svg
               width="25"
@@ -15,43 +15,43 @@ function Header() {
             >
               <path d="M1664 1504v-768q-32 36-69 66-268 206-426 338-51 43-83 67t-86.5 48.5-102.5 24.5h-2q-48 0-102.5-24.5t-86.5-48.5-83-67q-158-132-426-338-37-30-69-66v768q0 13 9.5 22.5t22.5 9.5h1472q13 0 22.5-9.5t9.5-22.5zm0-1051v-24.5l-.5-13-3-12.5-5.5-9-9-7.5-14-2.5h-1472q-13 0-22.5 9.5t-9.5 22.5q0 168 147 284 193 152 401 317 6 5 35 29.5t46 37.5 44.5 31.5 50.5 27.5 43 9h2q20 0 43-9t50.5-27.5 44.5-31.5 46-37.5 35-29.5q208-165 401-317 54-43 100.5-115.5t46.5-131.5zm128-37v1088q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-1088q0-66 47-113t113-47h1472q66 0 113 47t47 113z"></path>
             </svg>
-            <span className="mt-1 ml-3 text-sm lg:hidden">
+            <span className="ml-3 mt-1 text-sm lg:hidden">
               SHREYANS.JAIN.13@GMAIL.COM
             </span>
           </div>
           <div className="flex items-center">
-            <nav className="items-center hidden text-xl uppercase font-sen  lg:flex">
+            <nav className="font-sen hidden items-center text-xl uppercase  lg:flex">
               <a
                 href="#"
-                className="flex px-6 py-4 uppercase font-JosefinSlab font-black"
+                className="flex px-6 py-4 font-JosefinSlab font-black uppercase"
               >
                 Home
               </a>
               <a
                 href="#"
-                className="flex px-6 py-4 font-JosefinSlab uppercase font-black"
+                className="flex px-6 py-4 font-JosefinSlab font-black uppercase"
               >
                 Skills
               </a>
-              <p className="font-FranchiseFree uppercase text-6xl px-8">
+              <p className="px-8 font-FranchiseFree text-6xl uppercase">
                 {" "}
                 shreyans jain{" "}
               </p>
               <a
                 href="#"
-                className="flex px-6 py-4 font-JosefinSlab uppercase font-black"
+                className="flex px-6 py-4 font-JosefinSlab font-black uppercase"
               >
                 Project
               </a>
               <a
                 href="#"
-                className="flex px-6 py-4 font-JosefinSlab uppercase font-black"
+                className="flex px-6 py-4 font-JosefinSlab font-black uppercase"
               >
                 Resume
               </a>
             </nav>
             <img
-              className="inline lg:hidden size-10"
+              className="inline size-10 lg:hidden"
               src="https://cdn.jsdelivr.net/gh/200-DevelopersFound/SnapStore@main/portfolio/images/hamburger.png"
               alt="hamburger"
             />
--- src/components/HeroContainer/HeroContainer.tsx
+++ src/components/HeroContainer/HeroContainer.tsx
@@ -7,14 +7,14 @@ function HeroContainer() {
       {/* <img className="inline border-solid border-2 border-white" src="https://cdn.jsdelivr.net/gh/200-DevelopersFound/SnapStore@main/portfolio/images/command-line1.png" alt="commandline_image" /> */}
       {/* <div className=" h-14 bg-orange-600"><span>some text which I will change in some time </span> </div> */}
 
-      <div className="inline-flex items-center float-right ">
+      <div className="float-right inline-flex items-center ">
         <img
           src="https://cdn.jsdelivr.net/gh/200-DevelopersFound/SnapStore@main/portfolio/images/command-line1.png"
           alt=""
-          className="border-solid border-2 border-white self-center h-12 shadow-[black_12px_0px_12px_2px] z-10"
+          className="z-10 h-12 self-center border-2 border-solid border-white shadow-[black_12px_0px_12px_2px]"
         />
-        <div className=" h-8 lg:w-72 md:w-64 sm:w-60 w-48 leading-8 bg-orange-600 rounded-r px-2 ">
-          <span className="px-4 font-JosefinSlab font-black whitespace-nowrap">
+        <div className=" h-8 w-48 rounded-r bg-orange-600 px-2 leading-8 sm:w-60 md:w-64 lg:w-72 ">
+          <span className="whitespace-nowrap px-4 font-JosefinSlab font-black">
             FULL STACK ENGINEER
           </span>
         </div>
@@ -27,31 +27,31 @@ function HeroContainer() {
       <div className="grid grid-cols-4 gap-0">
         <div className="col-span-3 inline-block">
           <img
-            className="rotate-[16deg] m-4 xl:inline-block hidden lg:h-64 md:h-60 sm:h-56 h-52 align-top "
+            className="m-4 hidden h-52 rotate-[16deg] align-top sm:h-56 md:h-60 lg:h-64 xl:inline-block "
             src="https://images.unsplash.com/photo-1518112166137-85f9979a43aa?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
-          <span className="font-FranchiseFree lg:text-13xl md:text-11xl sm:text-9xl text-7xl uppercase">
+          <span className="font-FranchiseFree text-7xl uppercase sm:text-9xl md:text-11xl lg:text-13xl">
             Shreyans
           </span>
         </div>
 
         <div className="col-span-1 row-span-2 ">
           <img
-            className="xl:inline-block hidden h-full"
+            className="hidden h-full xl:inline-block"
             src="https://images.unsplash.com/photo-1518112166137-85f9979a43aa?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
         </div>
 
         <div className="col-span-3">
           <img
-            className=" border-8 rounded border-white xl:inline-block hidden  lg:h-64 md:h-60 sm:h-56 h-52 lg:w-64 md:w-60 sm:w-56 w-52 m-4 align-top"
+            className=" m-4 hidden size-52 rounded border-8  border-white align-top sm:size-56 md:size-60 lg:size-64 xl:inline-block"
             src="https://images.unsplash.com/photo-1614529303797-815a283581cc?q=80&w=3135&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
           <img
-            className=" border-8 rounded border-white xl:inline-block hidden  lg:h-64 md:h-60 sm:h-56 h-52  lg:w-64 md:w-60 sm:w-56 w-52 m-4 align-top"
+            className=" m-4 hidden size-52 rounded border-8  border-white align-top sm:size-56 md:size-60  lg:size-64 xl:inline-block"
             src="https://images.unsplash.com/photo-1614529303797-815a283581cc?q=80&w=3135&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
-          <span className="m-4 font-FranchiseFree lg:text-13xl md:text-11xl sm:text-9xl text-7xl uppercase">
+          <span className="m-4 font-FranchiseFree text-7xl uppercase sm:text-9xl md:text-11xl lg:text-13xl">
             Jain
           </span>
         </div>
