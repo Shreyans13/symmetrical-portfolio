ec9e421a8f697489b5ad2ea451cdfbf77c0b9d62
shreyans13 <shreyans1313@gmail.com>
2024-03-09 15:55:37 +0530

    prettier added with tailwind-prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ['selector', '[data-mode="dark"]'],
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: ["selector", '[data-mode="dark"]'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     // backgroundColor: "#000000",
     extend: {
       fontFamily: {
         FranchiseFree: ["FranchiseFree"],
-        JosefinSlab: ["Josefin Slab", "serif"]
+        JosefinSlab: ["Josefin Slab", "serif"],
       },
       colors: {
         backgroundColor: "var(--color-background)",
-        textBackground: "var(--text-background)"
+        textBackground: "var(--text-background)",
       },
 
-      backgroundColor: '#000000',
+      backgroundColor: "#000000",
       fontSize: {
-        '10xl': '10rem',
-        '11xl': '12rem',
-        '12xl': '14rem',
-        '13xl': '16rem',
+        "10xl": "10rem",
+        "11xl": "12rem",
+        "12xl": "14rem",
+        "13xl": "16rem",
+        "14xl": "18rem",
+        "15xl": "20rem",
+        "16xl": "22rem",
+        "17xl": "24rem",
+        "18xl": "26rem",
+        "19xl": "28rem",
+        "20xl": "30rem",
       },
       animation: {
-        marquee: 'marquee 5s linear infinite',
-        marquee2: 'marquee2 5s linear infinite',
+        marquee: "marquee 5s linear infinite",
+        marquee2: "marquee2 5s linear infinite",
       },
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
         marquee2: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0%)' },
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
         },
       },
     },
   },
   plugins: [],
-}
-
+};
